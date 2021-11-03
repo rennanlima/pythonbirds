b9ba3f4a1b3ab92682034002ba2b6a1cc302c679
rennanlima <lima.rennan27@gmail.com>
2021-11-03 15:08:04 -0300

    Criação de atributo complexo (filhos).

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=23):
+    def __init__(self, *filhos, nome=None, idade=23):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__=='__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Rennan'
-    print(p.nome)
-    print(p.idade)
+    martinho = Pessoa(nome='Martinho')
+    rennan = Pessoa(martinho, nome='Rennan')
+
+    print(Pessoa.cumprimentar(rennan))
+    print(id(rennan))
+    print(rennan.cumprimentar())
+    print(rennan.nome)
+    print(rennan.idade)
+    for filho in rennan.filhos:
+        print(filho.nome)
