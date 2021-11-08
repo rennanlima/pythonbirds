e6ab5b5d2d4f9a514b33ee4ca3d7827cfa7972e4
rennanlima <lima.rennan27@gmail.com>
2021-11-08 09:42:19 -0300

    Ajustes nos atores e obstáculos, finalização do teste_carro e sobrescritas de dados da classe.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -17,9 +17,17 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos = 3
+
 if __name__=='__main__':
-    martinho = Pessoa(nome='Martinho')
-    rennan = Pessoa(martinho, nome='Rennan')
+    martinho = Mutante(nome='Martinho')
+    rennan = Homem(martinho, nome='Rennan')
 
     print(Pessoa.cumprimentar(rennan))
     print(id(rennan))
@@ -34,9 +42,16 @@ if __name__=='__main__':
     del rennan.olhos
     print(rennan.__dict__)
     print(martinho.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(rennan.olhos)
     print(id(Pessoa.olhos), id(rennan.olhos))
     print(Pessoa.metodo_estatico(), rennan.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), rennan.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(rennan, Pessoa))
+    print(isinstance(rennan, Homem))
+    print(martinho.olhos)
+    print(rennan.cumprimentar())
+    print(martinho.cumprimentar())
